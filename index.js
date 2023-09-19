b2e5d207100b2828e9d5674d5d5a41bd34c6f3ee
Prince <princesolo919@gmail.com>
2023-09-19 23:01:46 +0100

    Initialize

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mongoose
 //middlewares
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"*",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
