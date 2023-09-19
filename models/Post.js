2f00009220e540ad5caa4e659a4427821380caed
Prince <princesolo919@gmail.com>
2023-09-19 20:09:46 +0100

    Initialize api

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose')
+
+const PostSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    desc:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    photo:{
+        type:String,
+        required:false,
+        
+    },
+    username:{
+        type:String,
+        required:true,  
+    },
+    userId:{
+        type:String,
+        required:true,  
+    },
+    categories:{
+        type:Array,       
+    },
+    views:{
+        type:Number,
+        default: 0
+    }
+},{timestamps:true})
+
+module.exports=mongoose.model("Post",PostSchema)
